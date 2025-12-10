29ea16cac69b461b7bb82f4caf84b72115305ef7
Anand Shukla <iamanand0666@gmail.com>
2025-12-10 11:06:26 +0530

    Update PayoutHistory.jsx

--- frontend/src/components/admin-panel/PayoutHistory.jsx
+++ frontend/src/components/admin-panel/PayoutHistory.jsx
@@ -14,7 +14,7 @@ const PayoutHistory = () => {
 
     const fetchHistory = async () => {
         try {
-            const { data } = await axios.get('http://localhost:5000/api/admin/payout-history', {
+            const { data } = await axios.get('https:backend.ranx24.com/api/admin/payout-history', {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('adminToken')}`
                 }
